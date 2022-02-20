6fdaa125a23381a0c283f579dedb690cff9e2c7b
sufiyan <shaikhsufiyan2003@gmail.com>
2022-02-20 20:46:32 +0530

    Routes mapped to login, index, forgot-password, signup pages.

--- app.js
+++ app.js
@@ -2,19 +2,37 @@ const express = require('express')
 const path = require('path')
 const router = require('./Routes/Routes')
 const mongoose = require('mongoose')
-
 const app = express()
 mongoose.connect('mongodb://localhost/Exam')
 app.use(express.urlencoded({extended:false}))
 const port = 3000
 app.use('/tests',router)
 app.set('view engine' , 'ejs')  // set view engine
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index' , {msg : "hello world"})
+   res.sendFile(path.join(__dirname, 'public/pages/index.html'));
 })
 
+app.get('/Login',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public/pages/login.html'));
 
+})
+
+app.get('/Signup',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public/pages/create-account.html'));
+
+})
+
+app.get('/create-account',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public/pages/create-account.html'));
+
+})
+
+app.get('/forgot-password',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public/pages/forgot-password.html'));
+
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
